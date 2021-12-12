449529cfa038545fc833b23492860ca439803598
Ignasi-Carreno <40855940+Ignasi-Carreno@users.noreply.github.com>
2021-12-12 22:11:47 +0100

    Refactor to split in separate zips

--- zipFiles.py
+++ zipFiles.py
@@ -8,28 +8,56 @@ from datetime import datetime
 """
 Zip files and upload it to s3
 """
-def zipFilesOld(s3_resource, bucket, files_to_zip, fileMonth, folder):
+def zipFiles(s3_resource, bucket, files_to_zip, fileMonth, folder):
 
+    MAX_SIZE = 314572800 # -> 300 mb
+    
+    currentSizeBytes = 0
+    tempFile = tempfile.NamedTemporaryFile('w', suffix='.tar.gz', delete=False)
+    zipFile = ZipFile(tempFile.name, 'w', compression=ZIP_DEFLATED, allowZip64=True)
     fileName =  fileMonth.strftime('%Y-%m') + '_' + str(int(datetime.now().timestamp()))
     
     # we download all files to tmp directory of lambda for that we create directory structure in /tmp same as s3 files structure (subdirectory)
     for KEY in files_to_zip:
-        try:
-            local_file_name = '/tmp/'+KEY
-            if not os.path.isdir(os.path.dirname(local_file_name)):
-              print("Making directory " + os.path.dirname(local_file_name))
-              os.makedirs(os.path.dirname(local_file_name))
+      try:
+          local_file_name = '/tmp/'+KEY
+          if not os.path.isdir(os.path.dirname(local_file_name)):
+            print("Making directory " + os.path.dirname(local_file_name))
+            os.makedirs(os.path.dirname(local_file_name))
+  
+          print('Downloading next file: ' + KEY)
+          s3_resource.Bucket(bucket).download_file(KEY, local_file_name)
+          
+          currentSizeBytes += os.path.getsize(local_file_name)
+          print('Current Size: ' + str(currentSizeBytes))
+          
+          if currentSizeBytes >= MAX_SIZE:
+            print('Max size reached, we will create new zip file')
+            zippedFileName = 'zipped/' + folder + fileName +'.tar.gz'
+            
+            print('Uploading current zip')
+            s3_resource.meta.client.upload_file(tempFile.name, bucket, zippedFileName)
+            
+            print('Removing current zip')
+            currentSizeBytes = 0
+            zipFile.close()
+            tempFile.close()
+            os.remove(tempFile.name)
+            
+            print('Making new zip')
+            tempFile = tempfile.NamedTemporaryFile('w', suffix='.tar.gz', delete=False)
+            zipFile = ZipFile(tempFile.name, 'w', compression=ZIP_DEFLATED, allowZip64=True)
+            fileName = fileMonth.strftime('%Y-%m') + '_' + str(int(datetime.now().timestamp()))
+          
+          zipFile.write(local_file_name)
+          os.remove(local_file_name)
+          
+      except ClientError as e:
+          print(e.response)
     
-            s3_resource.Bucket(bucket).download_file(KEY, local_file_name)
-        except ClientError as e:
-            print(e.response)
-
-    #now create empty zip file in /tmp directory use suffix .zip if you want 
-    with tempfile.NamedTemporaryFile('w', suffix='.tar.gz', delete=False) as f:
-      with ZipFile(f.name, 'w', compression=ZIP_DEFLATED, allowZip64=True) as zip:
-        for file in files_to_zip:
-          zip.write('/tmp/'+file)
-
-    #once zipped in temp copy it to your preferred s3 location 
+    #At the end of the bucle we also sent the last zip file to s3
+    print('Uploading last zip')
     zippedFileName = 'zipped/' + folder + fileName +'.tar.gz'
-    s3_resource.meta.client.upload_file(f.name, bucket, zippedFileName)
+    s3_resource.meta.client.upload_file(tempFile.name, bucket, zippedFileName)
+    zipFile.close()
+    tempFile.close()
